26315112ef7fbf9490cdede341bd5865df4572d8
Sezer Ökmen <szrokmn34@gmail.com>
2023-03-07 12:24:16 +0300

    privateRouter added

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -5,17 +5,25 @@ import About from "../pages/about/About";
 import Detail from "../pages/detail/Detail";
 import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
+import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
-      <GlobalStyles/>
+      <GlobalStyles />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
-        <Route path="about" element={<About />} />
-        <Route path="detail" element={<Detail />} />
+
+        <Route path="about" element={<PrivateRouter />}>
+          <Route path="" element={<About />} />
+        </Route>
+
+        <Route path="detail" element={<PrivateRouter />}>
+          <Route path="" element={<Detail />} />
+        </Route>
+        
       </Routes>
     </BrowserRouter>
   );
---
+++ src/router/PrivateRouter.jsx
@@ -0,0 +1,15 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+
+const PrivateRouter = () => {
+
+const user = false;
+
+  return (
+    <div>
+        { user ? <Outlet/> : <Navigate to={"/login"} /> }
+    </div>
+  )
+}
+
+export default PrivateRouter
